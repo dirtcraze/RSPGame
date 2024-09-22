96f645b79f1168bfa0b3ed39693d821ed280e1d4
dirtcraze <kamil.porebski@icloud.com>
2024-09-22 21:15:43 +0200

    Initial Commit

---
+++ RSPGame/RSPGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSPGameApp.swift
+//  RSPGame
+//
+//  Created by Kamil Porębski on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RSPGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RSPGameTests/RSPGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  RSPGameTests.swift
+//  RSPGameTests
+//
+//  Created by Kamil Porębski on 22/09/2024.
+//
+
+import Testing
+@testable import RSPGame
+
+struct RSPGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
